99c517147d79cd93c6bef46148bdd7b11710d3b1
Derek Wheelden <derek.wheelden@gmail.com>
2016-12-09 20:56:22 -0500

    Heroku STRICK

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const handlebars  = require('express-handlebars');
 const app = express();
 const posts = require('./app/posts.js');
-const hbs = hbs = handlebars.create({
+const hbs = handlebars.create({
     defaultLayout : 'main',
     extname : '.hbs',
     layoutsDir : 'views/layouts/',
